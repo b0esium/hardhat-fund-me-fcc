6550566151706856afdd45de55474280bb917bd8
b0esium <b0esium@protonmail.com>
2023-06-22 17:53:37 +0200

    Initial commit

---
+++ contracts/FundMe.sol
@@ -0,0 +1,99 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "./PriceConverter.sol";
+
+// Interfaces
+
+// Libraries
+
+// Error handling
+error FundMe__NotOwner();
+
+/// @title A contract for crowdfunding
+/// @author b0esium
+/// @notice This contract is to demo a sample funding contract
+/// @dev This implements price feeds as our library
+
+contract FundMe {
+    // Type declarations
+    using PriceConverter for uint256;
+
+    // State variables
+    mapping(address => uint256) private s_addressToAmountFunded;
+    address[] private s_funders;
+    address private immutable i_owner;
+    uint256 public constant MINIMUM_USD = 50 * 10 ** 18;
+    AggregatorV3Interface private s_priceFeed;
+
+    // Modifiers
+    modifier onlyOwner() {
+        // require(msg.sender == owner);
+        if (msg.sender != i_owner) revert FundMe__NotOwner();
+        _;
+    }
+
+    // Functions
+    constructor(address priceFeedAddress) {
+        i_owner = msg.sender;
+        s_priceFeed = AggregatorV3Interface(priceFeedAddress);
+    }
+
+    receive() external payable {
+        fund();
+    }
+
+    fallback() external payable {
+        fund();
+    }
+
+    /// @notice This function funds this contract
+    /// @dev This implements price feeds as our library
+
+    function fund() public payable {
+        require(
+            msg.value.getConversionRate(s_priceFeed) >= MINIMUM_USD,
+            "You need to spend more ETH!"
+        );
+        // require(PriceConverter.getConversionRate(msg.value) >= MINIMUM_USD, "You need to spend more ETH!");
+        s_addressToAmountFunded[msg.sender] += msg.value;
+        s_funders.push(msg.sender);
+    }
+
+    function withdraw() public payable onlyOwner {
+        address[] memory funders = s_funders;
+        // but mappings can't be in memory
+        for (
+            uint256 funderIndex = 0;
+            funderIndex < funders.length;
+            funderIndex++
+        ) {
+            address funder = funders[funderIndex];
+            s_addressToAmountFunded[funder] = 0;
+        }
+        s_funders = new address[](0);
+        (bool success, ) = i_owner.call{value: address(this).balance}("");
+        require(success, "Call failed");
+    }
+
+    // View/Pure (getter functions here)
+
+    function getOwner() public view returns (address) {
+        return i_owner;
+    }
+
+    function getFunder(uint256 index) public view returns (address) {
+        return s_funders[index];
+    }
+
+    function getAddressToAmountFunded(
+        address funder
+    ) public view returns (uint256) {
+        return s_addressToAmountFunded[funder];
+    }
+
+    function getPriceFeed() public view returns (AggregatorV3Interface) {
+        return s_priceFeed;
+    }
+}
